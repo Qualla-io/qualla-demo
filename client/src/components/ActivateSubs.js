3ec559c740e91ad3c51bce9416157814f94148e6
ghardin1314 <g.hardin1314@gmail.com>
2020-11-06 14:54:15 -0800

    added subscriber balance

--- client/src/components/ActivateSubs.js
+++ client/src/components/ActivateSubs.js
@@ -1,28 +1,74 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Button from "@material-ui/core/Button";
 import {useSelector} from "react-redux";
 import {useDispatch} from "react-redux";
 import axios from "axios";
 import {makeStyles} from "@material-ui/core/styles";
 import {useSnackbar} from "notistack";
-
+import {ethers} from "ethers";
 import * as creatorActions from "../store/actions/CreatorActions";
+import {Typography} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: theme.spacing(2),
+    display: "flex",
+    direction: "row",
+  },
+  btn: {},
+  balance: {
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    display: "flex",
+    direction: "column",
+    justify: "center",
+    alignItems: "center",
   },
 }));
 
 export default function ActivateSubs() {
+  const [allowance, setAllowance] = useState(0);
   const {enqueueSnackbar} = useSnackbar();
   const classes = useStyles();
   const web3State = useSelector((state) => state.Web3Reducer);
+  const creatorState = useSelector((state) => state.CreatorReducer);
 
   const dispatch = useDispatch();
 
   function updateContract() {
     dispatch(creatorActions.updateContract());
+  }
+
+  useEffect(() => {
+    subscribeAllowance();
+  }, [creatorState.contract]);
+
+  function subscribeAllowance() {
+    console.log("starting");
+    if (creatorState.contract.subscribers) {
+      let subsciberAddress = creatorState.contract.subscribers[0].subscriber;
+      let filterAllowance = web3State.Dai.filters.Approval(
+        subsciberAddress,
+        null
+      );
+
+      let filterTransfer = web3State.Dai.filters.Transfer(
+        subsciberAddress,
+        creatorState.contract.address
+      );
+
+      let handleApproval = async function (src, guy, wad) {
+
+        web3State.Dai.allowance(
+          subsciberAddress,
+          creatorState.contract.address
+        ).then((allowance) => {
+          setAllowance(ethers.utils.formatEther(allowance));
+        });
+      };
+
+      web3State.Dai.on(filterAllowance, handleApproval);
+      web3State.Dai.on(filterTransfer, handleApproval);
+    }
   }
 
   function activateSub() {
@@ -33,7 +79,7 @@ export default function ActivateSubs() {
     axios
       .post("http://localhost:8080/fakesub", {publisher: web3State.account})
       .then((res) => {
-        updateContract()
+        updateContract();
         enqueueSnackbar(res.data, {
           variant: "success",
           autoHideDuration: 2000,
@@ -42,7 +88,7 @@ export default function ActivateSubs() {
   }
 
   return (
-    <>
+    <div className={classes.root}>
       <Button
         variant="contained"
         color="secondary"
@@ -51,6 +97,9 @@ export default function ActivateSubs() {
       >
         Activate Subscriber!
       </Button>
-    </>
+      <div className={classes.balance}>
+        <Typography>Subscriber Balance: ${allowance}</Typography>
+      </div>
+    </div>
   );
 }
